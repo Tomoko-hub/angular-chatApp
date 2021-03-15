d42d371fdcb12b881a925767ed225911db6ce4ab
Tomoko <haisaitomoco@gmail.com>
2021-03-15 10:59:48 +0200

    Add signUp component https://tomoko-hub.github.io/angular-chatApp/

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -6,8 +6,8 @@ import { map } from 'rxjs/operators';
 import { Comment } from '../class/comment';
 import { User } from '../class/user';
 
-const CURRENT_USER: User = new User(1, '五十川 洋平');
-const ANOTHER_USER: User = new User(2, '竹井 賢治');
+const CURRENT_USER: User = new User(1, 'Tomoko Uehara');
+const ANOTHER_USER: User = new User(2, 'Mika Peisa');
 
 @Component({
   selector: 'ac-chat',
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,16 +3,19 @@ import { CommonModule } from '@angular/common';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { RouterModule } from '@angular/router';
 
 import { environment } from '../../environments/environment';
 import { HeaderComponent } from './components/header/header.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 
 
+
 @NgModule({
   declarations: [HeaderComponent, NotFoundComponent],
   imports: [
     CommonModule,
+    RouterModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAuthModule,
     AngularFireDatabaseModule
